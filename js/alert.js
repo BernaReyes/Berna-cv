bec42f447586d1910bb356d33478fae172ca58e7
BernaReyes <reyesbernardo51@gmail.com>
2023-12-06 19:57:40 -0600

    Modificacion en botones de contacto

--- js/alert.js
+++ js/alert.js
@@ -1,3 +1,15 @@
 function mostrarAlert() {
     window.alert("Estamos trabajando en ello!");
   }
+
+  //TOOGLE POPUP****************************
+  document.getElementById('togglePopup').addEventListener('click', function(event) {
+    event.preventDefault(); // Evita el comportamiento predeterminado del enlace
+    var popup = document.getElementById('miPopup');
+    popup.style.display = (popup.style.display === 'block') ? 'none' : 'block';
+  });
+
+  // Función para cerrar el popup
+  function cerrarPopup() {
+    document.getElementById('miPopup').style.display = 'none';
+  }
